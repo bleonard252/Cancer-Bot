f7e9c01099f05aaed8c306ded5b8c6830dd18899
bleonard252 <bleonard252@rocketmail.com>
2017-04-13 09:51:28 -0400

    Update discord_bot.js

--- discord_bot.js
+++ discord_bot.js
@@ -92,52 +92,6 @@ try{
 }
 
 var commands = {	
-	"alias": {
-		usage: "<name> <actual command>",
-		description: "Creates command aliases. Useful for making simple commands on the fly",
-		process: function(bot,msg,suffix) {
-			var args = suffix.split(" ");
-			var name = args.shift();
-			if(!name){
-				msg.channel.sendMessage(Config.commandPrefix + "alias " + this.usage + "\n" + this.description);
-			} else if(commands[name] || name === "help"){
-				msg.channel.sendMessage("overwriting commands with aliases is not allowed!");
-			} else {
-				var command = args.shift();
-				aliases[name] = [command, args.join(" ")];
-				//now save the new alias
-				require("fs").writeFile("./alias.json",JSON.stringify(aliases,null,2), null);
-				msg.channel.sendMessage("created alias " + name);
-			}
-		}
-	},
-	"aliases": {
-		description: "lists all recorded aliases",
-		process: function(bot, msg, suffix) {
-			var text = "current aliases:\n";
-			for(var a in aliases){
-				if(typeof a === 'string')
-					text += a + " ";
-			}
-			msg.channel.sendMessage(text);
-		}
-	},
-    "idle": {
-	usage: "[status]",
-        description: "sets bot status to idle",
-        process: function(bot,msg,suffix){ 
-	    bot.user.setStatus("idle");
-	    bot.user.setGame(suffix);
-	}
-    },
-    "online": {
-	usage: "[status]",
-        description: "sets bot status to online",
-        process: function(bot,msg,suffix){ 
-	    bot.user.setStatus("online");
-	    bot.user.setGame(suffix);
-	}
-    },
     "say": {
         usage: "<message>",
         description: "bot says message",
