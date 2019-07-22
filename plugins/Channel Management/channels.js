6feb7eafbacbc09892d87609089cac71fe2ab2ba
bleonard252 <bleonard252@rocketmail.com>
2019-07-22 13:16:01 -0400

    Update channels.js

--- plugins/Channel Management/channels.js
+++ plugins/Channel Management/channels.js
@@ -17,7 +17,7 @@ exports.create = {
     process: function (bot, msg, suffix) {
         let xrandr = crypto.randomBytes(3).toString('hex');
         msg.channel.guild.createChannel("tmp" + xrandr, "text").then(function (channel) {
-	    if (msg.channel.guild.id == 602544926051270676) channel.edit({parent: "602901288119697408"});
+	    if (msg.channel.guild.id == 602544926051270676) {channel.edit({parent: "602901288119697408"});channel.edit({permissionOverwrites:channel.parent.permissionOverwrites});}
 	    channel.overwritePermissions(channel.guild.defaultRole, { "VIEW_CHANNEL": false, "READ_MESSAGES": false });
             channel.overwritePermissions(msg.author, { "SEND_TTS_MESSAGES": true, "MANAGE_MESSAGES": true, "VIEW_CHANNEL": true, "READ_MESSAGES": true });
             channel.overwritePermissions(bot.user, { "SEND_TTS_MESSAGES": false, "MANAGE_MESSAGES": true, "VIEW_CHANNEL": true, "READ_MESSAGES": true });
