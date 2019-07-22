c8cca0e2fc8cf32ddb7b2ce88174cf58bc8d7dd1
bleonard252 <bleonard252@rocketmail.com>
2019-07-22 12:56:49 -0400

    Update channels.js

--- plugins/Channel Management/channels.js
+++ plugins/Channel Management/channels.js
@@ -22,7 +22,7 @@ exports.create = {
             channel.overwritePermissions(bot.user, { "SEND_TTS_MESSAGES": false, "MANAGE_MESSAGES": true, "VIEW_CHANNEL": true, "READ_MESSAGES": true });
             msg.channel.send("created " + channel);
             channel.setTopic(suffix);
-	    if (msg.channel.guild.id == 602544926051270676) channel.parentID = 602901288119697408;
+	    if (msg.channel.guild.id == 602544926051270676) channel.edit({parent:602901288119697408});
         }).catch(function (error) {
             msg.channel.send("failed to create channel: " + error);
         });
